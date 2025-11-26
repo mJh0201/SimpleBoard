1c21dd0fe69f5c325ad552f96740a9f7de809b29
mJh0201 <ggb05091@naver.com>
2025-11-26 17:05:46 +0900

    feat: create + insert 추가 수정
    개요
    코드 합친거 마저 정리.

--- src/board/Controller/BoardController.java
+++ src/board/Controller/BoardController.java
@@ -1,12 +1,7 @@
 package board.Controller;
 
-import java.sql.Date;
-import java.time.LocalDate;
 import java.util.Scanner;
-
-import board.DAO.BoardDAO;
 import java.util.List;
-
 import board.DTO.BoardDTO;
 import board.Service.BoardService;
 import board.View.BoardView;
@@ -14,9 +9,9 @@ import board.View.BoardView;
 public class BoardController {
 	static Scanner sc = new Scanner(System.in);
 	static BoardService boardService = new BoardService();
-	
+
 	public static void main(String[] args) {
-    List<BoardDTO> dtoList = service.selectAll();
+		List<BoardDTO> dtoList = boardService.selectAll();
 
 		BoardView.list(dtoList);
 
@@ -29,11 +24,12 @@ public class BoardController {
 
 			switch (job) {
 			case 1 -> {
+				contentCreate();
 			}
-			case 2 -> { //글보기
+			case 2 -> { // 글보기
 				BoardView.input("글번호입력(뒤로가기:0)>> ");
 				int board_id = Integer.parseInt(sc.nextLine());
-				BoardView.print(service.selectOne(board_id));
+				BoardView.print(boardService.selectOne(board_id));
 			}
 			case 99 -> {
 				isStop = true;
@@ -43,15 +39,11 @@ public class BoardController {
 			}
 			}
 		}
-		BoardDTO boardDTO = new BoardDTO();
-		
-		contentCreate();
-		BoardView.createBoardMenu(boardDTO);
 	}
-	
-	private static BoardDTO contentCreate() {	
+
+	private static BoardDTO contentCreate() {
 		BoardDTO boardDTO = new BoardDTO();
-		
+
 		BoardView.input("제목 : ");
 		String title = sc.nextLine();
 		BoardView.input("작성자 : ");
@@ -60,12 +52,12 @@ public class BoardController {
 		String content = sc.nextLine();
 		BoardView.input("비밀번호 : ");
 		String user_pw = sc.nextLine();
-		
+
 		boardDTO.setBoard_title(title);
 		boardDTO.setUser_name(user_name);
 		boardDTO.setBoard_content(content);
 		boardDTO.setUser_pw(user_pw);
-		
+
 		String message = boardService.boardCreate(boardDTO);
 		System.out.println("[확인용] " + message);
 		return boardDTO;

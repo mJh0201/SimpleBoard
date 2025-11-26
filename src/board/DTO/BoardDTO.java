34564fd42fc026c97b2c1a202aa9a07b536ae2fc
mJh0201 <ggb05091@naver.com>
2025-11-26 12:07:36 +0900

    DTO추가, ID,PW 변경

--- src/board/DTO/BoardDTO.java
+++ src/board/DTO/BoardDTO.java
@@ -1,5 +1,26 @@
 package board.DTO;
 
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter@Setter@ToString
 public class BoardDTO {
 
+	private Integer board_id;
+	private String board_title;
+	private String board_content;
+	private Date board_date;
+	private String user_name;
+	private String user_pw;
+	
 }
+
+

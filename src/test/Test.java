61d8bfadabeac3bdd78cb09a60d1c9f8d9198275
uk10105 <lykalso33@naver.com>
2025-11-27 11:57:07 +0900

    [add]content 여러줄 입력받기 코드 추가

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,12 +1,25 @@
 package test;
 
+import java.util.Scanner;
+
 public class Test {
 
-	public static void main2(String[] args) {
-		System.out.println("hihi~~");
-	}
+//	public static void main2(String[] args) {
+//		System.out.println("hihi~~");
+//	}
 	public static void main(String[] args) {
-		System.out.println("test-유경");
-
+		Scanner sc = new Scanner(System.in);
+		StringBuffer sb = new StringBuffer();
+		System.out.println("입력 : ");
+		String input = "";
+		
+		while (!input.equals("종료")) {
+			input = sc.nextLine();
+			sb.append(input);
+			sb.append("\n");
+		}
+		
+		sb.delete(sb.length()-3, sb.length());
+		System.out.println(sb.toString());
 	}
 }
